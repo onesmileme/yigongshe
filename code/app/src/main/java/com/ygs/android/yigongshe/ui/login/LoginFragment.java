5e8920ea504b722cb37a90860912de8e212a006b
guchunhui.gch <guchunhui.gch@alibaba-inc.com>
2018-06-16 10:45:33 +0800

    add login layout

---
+++ code/app/src/main/java/com/ygs/android/yigongshe/ui/login/LoginFragment.java
@@ -0,0 +1,17 @@
+package com.ygs.android.yigongshe.ui.login;
+
+import com.ygs.android.yigongshe.R;
+import com.ygs.android.yigongshe.ui.base.BaseFragment;
+
+public class LoginFragment extends BaseFragment {
+
+    public void initView(){
+
+    }
+
+    public int getLayoutResId(){
+        return R.layout.fragment_login;
+
+    }
+
+}
