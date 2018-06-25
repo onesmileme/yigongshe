9faa35e8e74cc5003ee465d55208752e6dd4789f
zhaoruichao <zhaoruichao@lianjia.com>
2018-06-25 18:43:32 +0800

    loading view

---
+++ code/app/src/main/java/com/ygs/android/yigongshe/ui/dynamic/DynamicDetailActivity.java
@@ -0,0 +1,22 @@
+package com.ygs.android.yigongshe.ui.dynamic;
+
+import com.ygs.android.yigongshe.R;
+import com.ygs.android.yigongshe.ui.base.BaseActivity;
+
+/**
+ * Created by ruichao on 2018/6/25.
+ */
+
+public class DynamicDetailActivity extends BaseActivity {
+  @Override protected void initIntent() {
+
+  }
+
+  @Override protected void initView() {
+
+  }
+
+  @Override protected int getLayoutResId() {
+    return R.layout.activity_dynamic_detail;
+  }
+}
