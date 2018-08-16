96f1b81aaa364b62f7e9f310e8d68a75cdfd5859
zhaoruichao <zhaoruichao@lianjia.com>
2018-08-16 17:33:52 +0800

    乱码

--- code/app/src/main/java/com/ygs/android/yigongshe/webview/WebViewActivity.java
+++ code/app/src/main/java/com/ygs/android/yigongshe/webview/WebViewActivity.java
@@ -1,94 +1,80 @@
 package com.ygs.android.yigongshe.webview;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
-
+import butterknife.BindView;
 import com.ygs.android.yigongshe.R;
 import com.ygs.android.yigongshe.ui.base.BaseActivity;
 import com.ygs.android.yigongshe.view.CommonTitleBar;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import butterknife.BindView;
-
 public class WebViewActivity extends BaseActivity {
 
-    public static final String TITLE_KEY = "title";
+  public static final String TITLE_KEY = "title";
 
-    public static final String LOCAL_HTML_PATH = "html_path";
+  public static final String LOCAL_HTML_PATH = "html_path";
 
-    public static final String URL_KEY = "url_key";
+  public static final String URL_KEY = "url_key";
 
-    @BindView(R.id.titleBar)
-    CommonTitleBar titleBar;
+  @BindView(R.id.titleBar) CommonTitleBar titleBar;
 
-    @BindView(R.id.webview)
-    WebView mWebView;
+  @BindView(R.id.webview) WebView mWebView;
 
-    @Override
-    protected void initIntent(Bundle bundle){
+  @Override protected void initIntent(Bundle bundle) {
 
-        String title = bundle.getString(TITLE_KEY);
-        titleBar.getCenterTextView().setText(title);
+    String title = bundle.getString(TITLE_KEY);
+    titleBar.getCenterTextView().setText(title);
 
+    String htmlPath = bundle.getString(LOCAL_HTML_PATH);
+    if (htmlPath != null) {
+      try {
+        InputStream inputStream = getResources().getAssets().open(htmlPath);
 
-        String htmlPath = bundle.getString(LOCAL_HTML_PATH);
-        if (htmlPath != null){
-            try {
-                InputStream inputStream = getResources().getAssets().open(htmlPath);
+        InputStreamReader reader = new InputStreamReader(inputStream);
 
-                InputStreamReader reader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
 
-                BufferedReader bufferedReader = new BufferedReader(reader);
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        do {
+          line = bufferedReader.readLine();
+          if (line != null) {
+            sb.append(line);
+          }
+        } while (line != null);
+        bufferedReader.close();
 
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-                do {
-                   line = bufferedReader.readLine();
-                   if (line != null){
-                       sb.append(line);
-                   }
-                }while (line != null);
-                bufferedReader.close();
+        mWebView.loadDataWithBaseURL(null, sb.toString(), "text/html", "utf-8", null);
+      } catch (IOException ioe) {
+        ioe.printStackTrace();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else {
+      String url = bundle.getString(URL_KEY);
+      if (!TextUtils.isEmpty(url)) {
+        mWebView.loadUrl(url);
+      }
+    }
+  }
 
-                mWebView.loadData(sb.toString(),"text/html","utf-8");
+  @Override protected void initView() {
 
-            }catch (IOException ioe){
-                ioe.printStackTrace();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }else{
-            String url = bundle.getString(URL_KEY);
-            if (!TextUtils.isEmpty(url)){
-                mWebView.loadUrl(url);
-            }
+    titleBar.setListener(new CommonTitleBar.OnTitleBarListener() {
+      @Override public void onClicked(View v, int action, String extra) {
+        if (action == CommonTitleBar.ACTION_LEFT_BUTTON) {
+          finish();
         }
-    }
+      }
+    });
+  }
 
-    @Override
-    protected void initView(){
-
-        titleBar.setListener(new CommonTitleBar.OnTitleBarListener() {
-            @Override
-            public void onClicked(View v, int action, String extra) {
-                if (action == CommonTitleBar.ACTION_LEFT_BUTTON){
-                    finish();
-                }
-            }
-        });
-    }
-
-    @Override
-    protected int getLayoutResId(){
-        return R.layout.activity_webiew;
-    }
-
+  @Override protected int getLayoutResId() {
+    return R.layout.activity_webiew;
+  }
 }
